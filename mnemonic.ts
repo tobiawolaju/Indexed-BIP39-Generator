581e641f905eb82a844a4e5b7f3dfe6b1910eefa
tobiawolaju <tobiawolaju@gamil.com>
2026-03-05 13:03:48 +0100

    add vercel api

---
+++ mnemonic.ts
@@ -0,0 +1,16 @@
+import { indexToMnemonic } from "../indexToMnemonic/src/index";
+
+export default function handler(req, res) {
+  const { index } = req.query;
+
+  if (!index) {
+    return res.status(400).json({ error: "index required" });
+  }
+
+  try {
+    const mnemonic = indexToMnemonic(BigInt(index));
+    res.status(200).json({ mnemonic });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
